1963b224852029f4e6ccc9f1b4de32a080fa041e
Hank <ggondol@hotmail.com>
2017-09-02 17:13:27 +0900

    타임 매니저 추가

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cTimeManager.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cTimeManager.cpp
@@ -17,26 +17,67 @@ HRESULT cTimeManager::Setup(void)
 	_timer = new cTimer;
 	_timer->Setup();
 
-	D3DXFONT_DESC desc;
-#pragma warning(disable : 4996)
-	ZeroMemory(&desc, sizeof(desc));
-	desc.CharSet = DEFAULT_CHARSET;
-	strcpy(desc.FaceName, "Consolas");
-	desc.Height = 20;
-	desc.Width = 10;
-	desc.Weight = FW_BOLD;
-	desc.Quality = DEFAULT_QUALITY;
-	desc.MipLevels = 1;
-	desc.Italic = 0;
-	desc.OutputPrecision = OUT_DEFAULT_PRECIS;
-	desc.PitchAndFamily = FF_DONTCARE;
-
-	D3DXCreateFontIndirect(GETDEVICE, &desc, &m_pFont);
-
-
-	
+	//기본 폰트
+	D3D::SetFont(m_pFont);
+		
 
 	return S_OK;
 
+}
 
+
+void cTimeManager::Release(void)
+{
+	if (_timer != NULL) SAFE_DELETE(_timer);
+}
+
+void cTimeManager::Update(float lock)
+{
+	if (_timer != NULL) _timer->tick(lock);
+}
+
+unsigned long cTimeManager::getFrame(void)
+{
+	return _timer->getFrameRate();
+}
+
+void cTimeManager::Render()
+{
+	char str[256];
+	std::string strFrame;
+
+	/*SetBkMode(hdc, TRANSPARENT);
+	SetTextColor(hdc, RGB(0, 0, 0, ));*/
+
+
+#pragma warning(disable : 4996)
+#ifdef _DEBUG
+	if (_timer != NULL)
+	{
+
+		sprintf(str, "framePerSec : %d", _timer->getFrameRate());
+		//TextOut(hdc, 0, 0, str, strlen(str));		
+		SetRect(&_rc, 0, 0, 800, 600);
+		m_pFont->DrawText(NULL, str, -1, &_rc, DT_LEFT, D3DCOLOR_ARGB(255, 255, 255, 255));
+
+
+		sprintf(str, "worldTime : %f", _timer->getWorldTime());
+		//TextOut(hdc, 0, 20, str, strlen(str));
+		SetRect(&_rc, 0, 20, 800, 600);
+		m_pFont->DrawText(NULL, str, -1, &_rc, DT_LEFT, D3DCOLOR_ARGB(255, 255, 255, 255));
+
+
+		sprintf(str, "elapsedTime : %f", _timer->getElapsedTime());
+		//TextOut(hdc, 0, 40, str, strlen(str));
+		SetRect(&_rc, 0, 40, 800, 600);
+		m_pFont->DrawText(NULL, str, -1, &_rc, DT_LEFT, D3DCOLOR_ARGB(255, 255, 255, 255));
+	}
+
+#else
+	if (_timer != NULL)
+	{
+		sprintf(str, "framePerSec : %d", _timer->getFrameRate());
+		TextOut(hdc, 0, 0, str, strlen(str));
+	}
+#endif
 }
