8942594252e10d5cf7b50d9858693fe0b9eb973e
Hank <ggondol@hotmail.com>
2017-09-02 16:31:54 +0900

    디바이스 까지 추가 완료 씬메니저 추가가 필요

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cMainGame.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cMainGame.cpp
@@ -25,11 +25,22 @@ void cMainGame::Release()
 
 void cMainGame::Render()
 {
-	
+	GETDEVICE->Clear(NULL, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(47, 121, 112), 1.0f, 1);
+	GETDEVICE->BeginScene();
+
+	//SCENEMANAGER->Render();
+	//TIMEMANAGER->Render();
+
+	GETDEVICE->EndScene();
+	GETDEVICE->Present(0, 0, 0, 0);
+
 }
 
 void cMainGame::Update()
 {
-	
+	cGameNode::Update();
+
+	//SCENEMANAGER->Update();
+	//AUTORELEASEPOOL->Drain();
 }
 
