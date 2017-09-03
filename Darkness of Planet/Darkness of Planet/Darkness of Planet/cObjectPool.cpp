ed3d3a5283c61fac165a69b87dfdbfa2ee0d7a01
YeoHyeongu <hyeongu12@gmail.com>
2017-09-03 23:19:08 +0900

    오브젝트 풀 완성

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cObjectPool.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cObjectPool.cpp
@@ -10,3 +10,18 @@ cObjectPool::cObjectPool()
 cObjectPool::~cObjectPool()
 {
 }
+
+void cObjectPool::AddObject(cObject * pObject)
+{
+	m_setObject.insert(pObject);
+}
+
+void cObjectPool::RemoveObject(cObject * pObject)
+{
+	m_setObject.erase(pObject);
+}
+
+void cObjectPool::Destroy()
+{
+	//assert(m_setObject.empty() && "cObject Memory Leak");
+}
