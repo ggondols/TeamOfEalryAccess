06cb9bd378bb8679c1a7f58834480885977fb0b1
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-03 14:14:31 +0900

    키 매니저가 제대로 들어가지 않아 다시 추가합니다 이 전 작업과 동일합니다

---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cKeyManager.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "cSingletonBase.h"
+#define KEYMAX 256
+#include <bitset>
+/////////////////////////////////////////////////////////
+/////// 2017.09.03  김태영 //////////////////////////////
+////////// 키 매니저 ///////////////////////////////////
+//////키 누르는거 관리하는 매니저 입니다 ///////////////
+////////////////////////////////////////////////////////
+class cKeyManager:public cSingletonBase <cDeviceManager>
+{
+private:
+	bitset<KEYMAX> _keyUp;
+	bitset<KEYMAX> _keyDown;
+public:
+	HRESULT init(void);
+	void release(void);
+
+	//한번만 눌렀을때..
+	bool isOnceKeyDown(int key);
+
+	//한번만 떼었을때..
+	bool isOnceKeyUp(int key);
+
+	//계속 누르고 있을때..
+	bool isStayKeyDown(int key);
+
+	//딸깍 on 딸깍 off ~~~~~ (전등 스위치 개념...)
+	bool isToggleKey(int key);
+
+	bitset<KEYMAX> getKeyUp() { return _keyUp; }
+	bitset<KEYMAX> getKeyDown() { return _keyDown; }
+
+	void setKeyDown(int key, bool state) { _keyDown.set(key, state); }
+	void setKeyUp(int key, bool state) { _keyUp.set(key, state); }
+
+	cKeyManager();
+	~cKeyManager();
+};
+
