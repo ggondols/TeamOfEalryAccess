3c4d43419cc6aa50ff04ec006a9cdfa50c4a57b9
YeoHyeongu <hyeongu12@gmail.com>
2017-09-04 00:10:55 +0900

    게임 오브젝트 추가(임시)

---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cGameObject.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class cGameObject : public cObject
+{
+protected:
+	D3DXVECTOR3	m_vPrevPosition;
+	SYNTHESIZE(D3DXMATRIX, m_matWorld, WorldMatrix);
+	SYNTHESIZE(D3DXVECTOR3, m_vPos, Position);
+	SYNTHESIZE(D3DXVECTOR3, m_vDir, Direction);
+	//SYNTHESIZE_ADD_REF(cAction*, m_pAction, Action);
+	SYNTHESIZE(bool, m_isDirection, IsDirection);
+
+public:
+	cGameObject(void);
+	virtual ~cGameObject(void);
+
+	virtual void Update();
+};
+
