adedc35245ed219820d6b4fa13a2d41dface1f06
YeoHyeongu <hyeongu12@gmail.com>
2017-09-04 00:05:03 +0900

    오토 릴리즈 풀 완성

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cAutoReleasePool.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cAutoReleasePool.h
@@ -1,9 +1,25 @@
 #pragma once
 #include "cSingletonBase.h"
+#include "cObject.h"
 class cAutoReleasePool : public cSingletonBase<cAutoReleasePool>
 {
+protected:
+	set<cObject*> m_setPool;
 public:
 	cAutoReleasePool();
 	~cAutoReleasePool();
+
+	void AddObject(cObject* pObject)
+	{
+		m_setPool.insert(pObject);
+	}
+	void Drain()
+	{
+		for each(auto p in m_setPool)
+		{
+			p->Release();
+		}
+		m_setPool.clear();
+	}
 };
 
