b73147798c08bcb1cb930225dcc13fae8ec7037f
정현 <1meteor1@naver.com>
2017-09-04 02:39:55 +0900

    높이맵 생성

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cHeightMap.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cHeightMap.h
@@ -1,8 +1,21 @@
 #pragma once
 class cHeightMap
 {
+private:
+	LPD3DXMESH			m_pMesh;
+	string				m_sTexture;
+	D3DMATERIAL9		m_stMtl;
+	int					m_nTile;
+	vector<D3DXVECTOR3>	m_vecVertex;
 public:
-	cHeightMap();
-	~cHeightMap();
+	cHeightMap(void);
+	virtual ~cHeightMap(void);
+
+	virtual void Load(char* szFolder,
+		char* szFile,
+		char* szTexture,
+		DWORD dwBytesPerPixel = 1);
+
+	void Render();
 };
 
