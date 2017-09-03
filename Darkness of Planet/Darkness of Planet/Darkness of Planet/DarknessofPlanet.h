c022dc9456e5ac8a8e4caa52b9782a4c30ebc469
YeoHyeongu <hyeongu12@gmail.com>
2017-09-03 16:09:58 +0900

    텍스쳐 매니저 추가

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/DarknessofPlanet.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/DarknessofPlanet.h
@@ -1,4 +1,3 @@
 #pragma once
 
 #include "resource.h"
-//드래곤볼 4성구
---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cTextureManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "cSingletonBase.h"
+class cTextureManager : public cSingletonBase<cTextureManager>
+{
+public:
+	cTextureManager();
+	~cTextureManager();
+};
+
