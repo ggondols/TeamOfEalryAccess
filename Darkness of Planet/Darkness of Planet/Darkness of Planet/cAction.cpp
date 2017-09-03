35453cd870cf13a8f8e8d5a88cdee278c0c77c63
YeoHyeongu <hyeongu12@gmail.com>
2017-09-04 00:25:55 +0900

    액션 완성

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cAction.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cAction.cpp
@@ -1,12 +1,25 @@
-#include "stdafx.h"
+#include "StdAfx.h"
 #include "cAction.h"
 
 
-cAction::cAction()
+cAction::cAction(void)
+	: m_fPassedActionTime(0.0f)
+	, m_fActionTime(0.0f)
+	, m_pTarget(NULL)
+	, m_pDelegate(NULL)
 {
 }
 
-
-cAction::~cAction()
+cAction::~cAction(void)
 {
+}
+
+void cAction::Start()
+{
+	m_fPassedActionTime = 0.0f;
+}
+
+void cAction::Update()
+{
+
 }
--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cAction.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cAction.h
@@ -1,9 +1,27 @@
 #pragma once
-#include "cObject.h"
-class cAction : public cObject
+
+class cGameObject;
+class cAction;
+
+class iActionDelegate
 {
 public:
-	cAction();
-	~cAction();
+	virtual void OnFinish(cAction* pSender) = 0;
+};
+
+class cAction : public cObject
+{
+protected:
+	float m_fPassedActionTime;
+	SYNTHESIZE(float, m_fActionTime, ActionTime);
+	SYNTHESIZE(cGameObject*, m_pTarget, Target);
+	SYNTHESIZE(iActionDelegate*, m_pDelegate, Delegate);
+
+public:
+	cAction(void);
+	virtual ~cAction(void);
+
+	virtual void Start();
+	virtual void Update();
 };
 
