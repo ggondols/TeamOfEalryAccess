c91dd85dd56cda5545353160856b48085ccd6f2d
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-10 23:35:02 +0900

    물리적인 이동인 TeicPhysicsCrtctrl 클래스 완성 Getposition 과 Getangle로 캐릭터 위치와 회전 각도 얻는다 -by 태영

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicPhysicsCrtCtrl.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicPhysicsCrtCtrl.cpp
@@ -3,10 +3,107 @@
 
 
 TeicPhysicsCrtCtrl::TeicPhysicsCrtCtrl()
+	: m_vDir(0, 0, 1)
+	, m_vPos(0, 0, 0)
+	, m_fSpeed(0.0f)
+	, m_fAngle(0.0f)
+	, m_bMoving(false)
+	, m_fAcceleration(0.0f)
+	, m_bAttacking(false)
 {
+	D3DXMatrixIdentity(&m_matWorld);
 }
 
 
 TeicPhysicsCrtCtrl::~TeicPhysicsCrtCtrl()
 {
+}
+
+void TeicPhysicsCrtCtrl::Update()
+{
+
+	if (KEYMANAGER->isOnceKeyDown('L'))
+	{
+		m_bAttacking = true;
+		m_fSpeed = 0;
+	}
+
+	if (!m_bAttacking)
+	{
+		if (KEYMANAGER->isStayKeyDown('W'))
+		{
+
+			m_fAcceleration += 0.01f;
+			m_bMoving = true;
+
+
+		}
+		if (KEYMANAGER->isStayKeyDown('S'))
+		{
+			m_fAcceleration -= 0.01f;
+			//m_vPos = m_vPos + m_vDir * m_fSpeed;
+
+		}
+		if (KEYMANAGER->isStayKeyDown('A'))
+		{
+			m_fAngle -= 0.1f;
+
+
+		}
+
+		if (KEYMANAGER->isStayKeyDown('D'))
+		{
+			m_fAngle += 0.1f;
+
+
+		}
+	}
+
+
+	m_fAcceleration -= 0.0025f;
+	if (m_fAcceleration > 0.01) m_fAcceleration = 0.01f;
+	m_fSpeed += m_fAcceleration;
+	if (m_fSpeed > 1)m_fSpeed = 1;
+	if (m_fSpeed < 0)
+	{
+		m_fSpeed = 0;
+		m_bMoving = false;
+		m_fAcceleration = 0;
+	}
+	//m_vPos = m_vPos + m_vDir * m_fSpeed;
+	m_vPos.x += cosf(m_fAngle) * m_fSpeed;
+	m_vPos.z -= sinf(m_fAngle) * m_fSpeed;
+
+
+	m_vDir = D3DXVECTOR3(0, 0, 1);
+	D3DXMATRIX matR;
+	D3DXMatrixRotationY(&matR, m_fAngle);
+	D3DXVec3TransformNormal(&m_vDir, &m_vDir, &matR);
+
+
+	D3DXMATRIX matT;
+	D3DXMatrixTranslation(&matT, m_vPos.x, m_vPos.y, m_vPos.z);
+
+
+
+
+
+	m_matWorld =/* matR **/ matT;
+
+}
+
+
+D3DXVECTOR3* TeicPhysicsCrtCtrl::GetPosition()
+{
+	return &m_vPos;
+}
+
+D3DXVECTOR3 * TeicPhysicsCrtCtrl::GetPositionFuture()
+{
+	return &m_vFuturePos;
+}
+
+D3DXVECTOR3 * TeicPhysicsCrtCtrl::GetPositionPast()
+{
+	return &m_vPastPos;
 }
--- Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicPhysicsCrtCtrl.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicPhysicsCrtCtrl.h
@@ -1,6 +1,34 @@
 #pragma once
 class TeicPhysicsCrtCtrl
 {
+private:
+
+	D3DXVECTOR3				m_vPos;
+	D3DXVECTOR3				m_vFuturePos;
+	D3DXVECTOR3				m_vPastPos;
+	float					m_fSpeed;
+	float					m_fAcceleration;
+	float					m_fAngle;
+	bool					m_bMoving;
+	bool					m_bAttacking;
+public:
+
+protected:
+	SYNTHESIZE_PASS_BY_REF(D3DXMATRIX, m_matWorld, WorldTM);
+	D3DXVECTOR3				m_vDir;
+public:
+
+
+	void Update();
+
+	D3DXVECTOR3* GetPosition();
+	D3DXVECTOR3* GetPositionFuture();
+	D3DXVECTOR3* GetPositionPast();
+	bool getMoving() { return m_bMoving; };
+	bool getAttacking() { return m_bAttacking; }
+	void setAttacking(bool on) { m_bAttacking = on; }
+	void setMoving(bool on) { m_bMoving = on; }
+	float getAngle() { return m_fAngle; }
 public:
 	TeicPhysicsCrtCtrl();
 	~TeicPhysicsCrtCtrl();
