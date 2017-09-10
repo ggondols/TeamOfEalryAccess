7032bfd5c1083d064c3e965cad0df333222184d8
Hank <ggondol@hotmail.com>
2017-09-11 03:07:39 +0900

    노드 ver0.1 완성

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/HankcNode.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/HankcNode.cpp
@@ -22,7 +22,8 @@ nPositionInfo::nPositionInfo(int fx, int fz, int size)
 	// 0---1
 	// | / |
 	// 2---3
-	//012 - 213
+	// 012 - 213
+	// imap 역수로 전환 해야함
 	nodeVertex.push_back(D3DXVECTOR3(0, 0, 0));
 	nodeVertex.push_back(D3DXVECTOR3(1 * size, 0, 0));
 	nodeVertex.push_back(D3DXVECTOR3(0, 0, -1 * size));
@@ -30,11 +31,11 @@ nPositionInfo::nPositionInfo(int fx, int fz, int size)
 
 	m_vPos.x = fx * size;
 	m_vPos.y = 0;
-	m_vPos.z = fz * size;
+	m_vPos.z = -fz * size;
 
 	m_vCenterPos.x = fx * size + (size / 2);
 	m_vCenterPos.y = 0;
-	m_vCenterPos.z = fz * size + (size / 2);
+	m_vCenterPos.z = -fz * size + (size / 2);
 
 	m_vecVertex.push_back(nodeVertex[0]);
 	m_vecVertex.push_back(nodeVertex[1]);
