6502236b5a864e2732aeb8e6b28b61df63f89a45
정현 <1meteor1@naver.com>
2017-09-11 01:09:16 +0900

    캐릭터 없이 맵 위에서 카매라 이동

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cJustTestScene.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cJustTestScene.cpp
@@ -2,9 +2,11 @@
 #include "cJustTestScene.h"
 #include "iMap.h"
 #include "cHeightMap.h"
+#include "TeicPhysicsCrtCtrl.h"
 
 cJustTestScene::cJustTestScene()
 	: m_pMap(NULL)
+	, m_pCtrl(NULL)
 {
 }
 
@@ -16,7 +18,9 @@ HRESULT cJustTestScene::Setup()
 {
 	m_pCamera = new Hank::cCamera;
 	m_pGrid = new Hank::cGrid;
-	m_pCamera->Setup();
+	m_pCtrl = new TeicPhysicsCrtCtrl;
+	
+	m_pCamera->Setup(m_pCtrl->GetPosition());
 	m_pGrid->Setup();
 	cHeightMap* pHeightMap = new cHeightMap;
 	pHeightMap->Load("map/", "HeightMap.raw", "terrain.jpg");
@@ -28,11 +32,15 @@ HRESULT cJustTestScene::Setup()
 void cJustTestScene::Release()
 {
 	m_pGrid->Release();
+	SAFE_DELETE(m_pCtrl);
 	SAFE_DELETE(m_pMap);
+	SAFE_DELETE(m_pCamera);
 }
 
 void cJustTestScene::Update()
 {
+	m_pCtrl->Update();
+	m_pMap->GetHeight(m_pCtrl->GetPosition()->x, m_pCtrl->GetPosition()->y, m_pCtrl->GetPosition()->z);
 	m_pCamera->Update();
 }
 
--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cJustTestScene.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cJustTestScene.h
@@ -4,14 +4,16 @@
 #include "HankcGrid.h"
 
 class iMap;
+class TeicPhysicsCrtCtrl;
 
 class cJustTestScene : public cGameNode
 {
 private:
-	Hank::cCamera *m_pCamera;
-	Hank::cGrid *m_pGrid;
+	Hank::cCamera*			m_pCamera;
+	Hank::cGrid*			m_pGrid;
 
-	iMap*			m_pMap;
+	iMap*					m_pMap;
+	TeicPhysicsCrtCtrl*		m_pCtrl;
 
 public:
 	cJustTestScene();
