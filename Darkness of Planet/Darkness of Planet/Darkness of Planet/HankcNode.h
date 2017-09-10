6e9cab935d912e2f39ea4a00508d03042ed870f4
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-10 23:43:34 +0900

    물리적인 컨트롤 클래스 상단에 만든 날짜, 설명 등 빼먹은 정보 추가

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/HankcNode.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/HankcNode.h
@@ -40,13 +40,6 @@ public:
 	vector<D3DXVECTOR3> m_vecVertex;
 };
 
-class nAstarInfo
-{
-public:
-	bool m_isOpen;
-	bool m_isClose;
-};
-
 //노드가 가지고 있을 충돌체 정보 리스트
 class nNodeBoundInfo
 {
@@ -54,8 +47,6 @@ public:
 	list<BoundingSquare*> m_vecBounding;
 };
 
-
-//노드 핵심 정의
 class HankcNode : nInterface
 {
 public:
@@ -64,33 +55,14 @@ public:
 	int m_nIndex;
 	
 public:
-	//link function // 인터페이스 타입, GRID의 사각형 점, 노드위에 있는 충돌체들 정보 // astar용 
+	//link function
 	nInterface *m_pInterface;
 	nPositionInfo *m_vPosList;	
 	nNodeBoundInfo *m_pBoundInfo;
 
+
 public:
+
 	HankcNode();
 	~HankcNode();
-};
-
-//열정보
-class HankcNodeRow
-{
-public:
-
-	vector<HankcNode> m_vRow;
-
-public:
-	HankcNode* operator [] (int row)
-	{
-		return &m_vRow[row];
-	}
-};
-
-//행열
-class HankcGrid
-{
-public:
-	vector<HankcNodeRow> m_vCol;
 };
