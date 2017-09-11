db76a4f7008c82625a0d7b729e2396f58dcc88f1
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-11 17:45:34 +0900

    충돌하면 잠시 멈추는 TeicCollisionMove 클래스 생성했습니다 -김태영

---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicCollisionMove.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "TeicCollisionMove.h"
+
+
+TeicCollisionMove::TeicCollisionMove()
+{
+}
+
+
+TeicCollisionMove::~TeicCollisionMove()
+{
+}
---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicCollisionMove.h
@@ -0,0 +1,8 @@
+#pragma once
+class TeicCollisionMove
+{
+public:
+	TeicCollisionMove();
+	~TeicCollisionMove();
+};
+
