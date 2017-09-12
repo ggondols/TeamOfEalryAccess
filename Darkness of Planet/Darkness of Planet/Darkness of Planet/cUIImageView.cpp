4e366b433cc731145f9faa3e53301ef2b6dbe9c5
정현 <1meteor1@naver.com>
2017-09-12 15:10:39 +0900

    FontManager 생성 및 추가, UI Image, Text View 클래스 내용 채워넣음

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cUIImageView.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cUIImageView.cpp
@@ -1,12 +1,35 @@
 #include "stdafx.h"
 #include "cUIImageView.h"
 
-
-cUIImageView::cUIImageView()
+cUIImageView::cUIImageView(void)
 {
 }
 
-
-cUIImageView::~cUIImageView()
+cUIImageView::~cUIImageView(void)
 {
+}
+
+void cUIImageView::SetTexture(string sFullPath)
+{
+	m_sTexture = sFullPath;
+}
+
+void cUIImageView::Render(LPD3DXSPRITE pSprite)
+{
+	D3DXIMAGE_INFO stImageInfo;
+	LPDIRECT3DTEXTURE9 pTexture = TEXTUREMANAGER->GetTextureEx(m_sTexture, &stImageInfo);
+	pSprite->Begin(D3DXSPRITE_ALPHABLEND | D3DXSPRITE_SORT_TEXTURE);
+
+	pSprite->SetTransform(&m_matWorld);
+	RECT rc;
+	SetRect(&rc, 0, 0, stImageInfo.Width, stImageInfo.Height);
+	pSprite->Draw(pTexture,
+		&rc,
+		&D3DXVECTOR3(0, 0, 0),
+		&D3DXVECTOR3(0, 0, 0),
+		D3DCOLOR_ARGB(255, 255, 255, 255));
+
+	pSprite->End();
+
+	cUIObject::Render(pSprite);
 }
--- Darkness of Planet/Darkness of Planet/Darkness of Planet/cUITextView.h
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cUITextView.h
@@ -1,10 +1,18 @@
 #pragma once
-#include "cUIObject.h"
 
 class cUITextView : public cUIObject
 {
+protected:
+	
+	SYNTHESIZE(string, m_sText, Text);
+	SYNTHESIZE(cFontManager::eFontType, m_eFontType, FontType);
+	SYNTHESIZE(DWORD, m_dwDT, DrawTextFormat);
+	SYNTHESIZE(D3DCOLOR, m_dwColor, Color);
 public:
-	cUITextView();
-	~cUITextView();
+	cUITextView(void);
+	virtual ~cUITextView(void);
+
+	// cUIObject override
+	virtual void Render(LPD3DXSPRITE pSprite) override;
 };
 
