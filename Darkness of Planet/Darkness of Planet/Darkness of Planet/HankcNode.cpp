6e7d9e074bef5f7723a2d6356069f5cdcb53cb7a
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-13 02:30:56 +0900

    전체화면 모드로 변경 가능하게 바꿔 놓음 (전체화면 테스트 중 HankcNode 에서 에러가 날 시 컴퓨터를 꺼야 해서 임시로 막아놓음) 전체화면 모드로 하면 디버깅 에러가 발생하면 컴퓨터 꺼야하기 때문에 주석으로 전체화면으로 돌릴 수 있게만 해놓고 원래 버전으로 바꿔놓음

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/HankcNode.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/HankcNode.cpp
@@ -33,7 +33,7 @@ void HankcNode::InitFrame(int fx, int fz)
 HankcNode::~HankcNode()
 {
 	SAFE_DELETE(m_vPosList);
-	SAFE_DELETE(m_pInterface);
+//	SAFE_DELETE(m_pInterface);
 	SAFE_DELETE(m_pBoundInfo);
 }
 
