8f85ac243f3cdf21149f0a9f146be3fe16044455
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-21 10:15:12 +0900

    WayManager 생성 Astar 길찾기를 미리 로딩해서 가져다 쓰기 위한 매니저 입니다 (실시간 길 찾기가 아닌 처음에 여러마리가 길 따라 올때 사용합니다)

---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicWayManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "TeicWayManager.h"
+
+
+TeicWayManager::TeicWayManager()
+{
+}
+
+
+TeicWayManager::~TeicWayManager()
+{
+}
---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicWayManager.h
@@ -0,0 +1,10 @@
+#pragma once
+class TeicWayManager
+{
+private:
+	vector<D3DXVECTOR3> mdsf;
+public:
+	TeicWayManager();
+	~TeicWayManager();
+};
+
