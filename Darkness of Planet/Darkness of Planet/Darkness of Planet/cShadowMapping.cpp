635404071f594f09e4307faac5dee2681ff82c1e
LeeDongYoun <dkdla12@naver.com>
2017-09-21 09:57:23 +0900

    쉐도우 매핑테스트클래스 추가함

---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cShadowMapping.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cShadowMapping.h"
+
+
+cShadowMapping::cShadowMapping()
+{
+}
+
+
+cShadowMapping::~cShadowMapping()
+{
+}
---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/cShadowMapping.h
@@ -0,0 +1,8 @@
+#pragma once
+class cShadowMapping
+{
+public:
+	cShadowMapping();
+	~cShadowMapping();
+};
+
