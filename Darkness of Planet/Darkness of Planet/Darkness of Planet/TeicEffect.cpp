09c3ee1c9163d7efa52dc30e10502234a7abb86f
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-26 20:53:35 +0900

    불꽃으로 이펙트 테스트 중입니다. (effect클래스)

--- Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicEffect.cpp
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicEffect.cpp
@@ -25,28 +25,26 @@ void TeicEffect::Setup(char * Textfilename, float size, int Xnum, int Ynum)
 	m_iSpriteXnum = Xnum;
 	m_iSpriteYnum = Ynum;
 
-	vector<ST_PT_VERTEX>	m_vecVertex;
-
-
+	vector<ST_PN_VERTEX>	m_vecVertex;
 	vector<D3DXVECTOR3>	vecVertex;
-	vector<D3DXVECTOR2> vecTex;
 	vector<DWORD>		vecIndex;
-	vecVertex.push_back(D3DXVECTOR3(-m_fSize, -m_fSize, -m_fSize));
-	vecVertex.push_back(D3DXVECTOR3(-m_fSize, m_fSize, -m_fSize));
-	vecVertex.push_back(D3DXVECTOR3(m_fSize, m_fSize, -m_fSize));
-	vecVertex.push_back(D3DXVECTOR3(m_fSize, -m_fSize, -m_fSize));
-	vecVertex.push_back(D3DXVECTOR3(-m_fSize, -m_fSize, m_fSize));
-	vecVertex.push_back(D3DXVECTOR3(-m_fSize, m_fSize, m_fSize));
-	vecVertex.push_back(D3DXVECTOR3(m_fSize, m_fSize, m_fSize));
-	vecVertex.push_back(D3DXVECTOR3(m_fSize, -m_fSize, m_fSize));
+	vecVertex.push_back(D3DXVECTOR3(-size, -size, 0));
+	vecVertex.push_back(D3DXVECTOR3(-size, size, 0));
+	vecVertex.push_back(D3DXVECTOR3(size, size, 0));
+	vecVertex.push_back(D3DXVECTOR3(size, -size, 0));
+	vecVertex.push_back(D3DXVECTOR3(-size, -size, 0));
+	vecVertex.push_back(D3DXVECTOR3(-size, size, 0));
+	vecVertex.push_back(D3DXVECTOR3(size, size, 0));
+	vecVertex.push_back(D3DXVECTOR3(size, -size, 0));
 
-	vecTex.push_back(D3DXVECTOR2( 0, 1));
-	vecTex.push_back(D3DXVECTOR2(0, 0));
-	vecTex.push_back(D3DXVECTOR2(1, 0));
+	vector<D3DXVECTOR3> vecNormal;
+	vecNormal.push_back(D3DXVECTOR3(0, 0, -1));
+	vecNormal.push_back(D3DXVECTOR3(0, 0, 1));
+	vecNormal.push_back(D3DXVECTOR3(-1, 0, 0));
+	vecNormal.push_back(D3DXVECTOR3(1, 0, 0));
+	vecNormal.push_back(D3DXVECTOR3(0, 1, 0));
+	vecNormal.push_back(D3DXVECTOR3(0, -1, 0));
 
-	vecTex.push_back(D3DXVECTOR2(0, 1));
-	vecTex.push_back(D3DXVECTOR2(1, 0));
-	vecTex.push_back(D3DXVECTOR2(1, 1));
 	//앞
 	vecIndex.push_back(0);
 	vecIndex.push_back(1);
@@ -55,73 +53,82 @@ void TeicEffect::Setup(char * Textfilename, float size, int Xnum, int Ynum)
 	vecIndex.push_back(2);
 	vecIndex.push_back(3);
 	////뒤
-	//vecIndex.push_back(4);
-	//vecIndex.push_back(6);
-	//vecIndex.push_back(5);
-	//vecIndex.push_back(4);
-	//vecIndex.push_back(7);
-	//vecIndex.push_back(6);
-	////좌
-	//vecIndex.push_back(4);
-	//vecIndex.push_back(5);
-	//vecIndex.push_back(1);
-	//vecIndex.push_back(4);
-	//vecIndex.push_back(1);
-	//vecIndex.push_back(0);
-	////우
-	//vecIndex.push_back(3);
-	//vecIndex.push_back(2);
-	//vecIndex.push_back(6);
-	//vecIndex.push_back(3);
-	//vecIndex.push_back(6);
-	//vecIndex.push_back(7);
-	////상
-	//vecIndex.push_back(1);
-	//vecIndex.push_back(5);
-	//vecIndex.push_back(6);
-	//vecIndex.push_back(1);
-	//vecIndex.push_back(6);
-	//vecIndex.push_back(2);
-	////하
-	//vecIndex.push_back(4);
-	//vecIndex.push_back(0);
-	//vecIndex.push_back(3);
-	//vecIndex.push_back(4);
-	//vecIndex.push_back(3);
-	//vecIndex.push_back(7);
-
+	
 	for (size_t i = 0; i < vecIndex.size(); ++i)
 	{
 		D3DXVECTOR3 p = vecVertex[vecIndex[i]];
-		D3DXVECTOR2	t = vecTex[i];
-		m_vecVertex.push_back(ST_PT_VERTEX(p, t));
+		D3DXVECTOR3 n = vecNormal[i / 6];
+		m_vecVertex.push_back(ST_PN_VERTEX(p, n));
+	}
+
+	D3DXMATRIX ro;
+	D3DXMatrixRotationY(&ro, D3DX_PI / 3.0);
+	for (size_t i = 0; i < vecIndex.size(); ++i)
+	{
+		D3DXVECTOR3 p = vecVertex[vecIndex[i]];
+		D3DXVec3TransformCoord(&p, &p, &ro);
+		D3DXVECTOR3 n = vecNormal[i / 6];
+		m_vecVertex.push_back(ST_PN_VERTEX(p, n));
+	}
+	
+	D3DXMatrixRotationY(&ro, D3DX_PI*2.0 / 3.0);
+	for (size_t i = 0; i < vecIndex.size(); ++i)
+	{
+		D3DXVECTOR3 p = vecVertex[vecIndex[i]];
+		D3DXVec3TransformCoord(&p, &p, &ro);
+		D3DXVECTOR3 n = vecNormal[i / 6];
+		m_vecVertex.push_back(ST_PN_VERTEX(p, n));
 	}
 
 
-	D3DXCreateMeshFVF(vecIndex.size() / 3,
+
+
+
+
+
+	D3DXCreateMeshFVF(m_vecVertex.size() / 3,
 		m_vecVertex.size(),
 		D3DXMESH_MANAGED | D3DXMESH_32BIT,
-		ST_PT_VERTEX::FVF,
+		ST_PN_VERTEX::FVF,
 		GETDEVICE,
 		&m_pMesh);
 
-	ST_PT_VERTEX* pV = 0;
+	ST_PN_VERTEX* pV = 0;
 	m_pMesh->LockVertexBuffer(0, (LPVOID*)&pV);
-	memcpy(pV, &m_vecVertex[0], m_vecVertex.size() * sizeof(ST_PT_VERTEX));
+	memcpy(pV, &m_vecVertex[0], m_vecVertex.size() * sizeof(ST_PN_VERTEX));
 	m_pMesh->UnlockVertexBuffer();
 
 	DWORD* pI = 0;
 	m_pMesh->LockIndexBuffer(0, (LPVOID*)&pI);
-	memcpy(pI, &vecIndex[0], vecIndex.size() * sizeof(DWORD));
+	for (int i = 0; i < m_vecVertex.size(); i++)
+	{
+		pI[i] = i;
+	}
+	
 	m_pMesh->UnlockIndexBuffer();
 
 	DWORD* pA = 0;
 	m_pMesh->LockAttributeBuffer(0, &pA);
-	ZeroMemory(pA, (vecIndex.size() / 3) * sizeof(DWORD));
+	for (int i = 0; i < m_vecVertex.size()/3; i++)
+	{
+		if (i < vecIndex.size() / 3.0)
+		{
+			pA[i] = 0;
+		}
+		else if (i < vecIndex.size()*2.0 / 3.0)
+		{
+			pA[i] = 1;
+		}
+		else 
+		{
+			pA[i] = 2;
+		}
+	}
+
 	m_pMesh->UnlockAttributeBuffer();
 
 
-	vector<DWORD> vecAdj(vecIndex.size());
+	vector<DWORD> vecAdj(m_vecVertex.size());
 	m_pMesh->GenerateAdjacency(0, &vecAdj[0]);
 
 	m_pMesh->OptimizeInplace(
@@ -147,8 +154,7 @@ void TeicEffect::Update()
 
 void TeicEffect::Render()
 {
-
-//	m_pMesh->DrawSubset(0);
+	
 	m_fTimeChange += 0.01;
 
 
@@ -173,25 +179,40 @@ void TeicEffect::Render()
 	m_pEffect->SetMatrix("matProjection", &matProjection);
 	m_pEffect->SetMatrix("matViewProjection", &(matView*matProjection));
 	m_pEffect->SetMatrix("matWorldViewProjection", &(matWorld*matView*matProjection));*/
-	m_pEffect->SetFloat("time_0_X", INF);
+	m_pEffect->SetFloat("Fire_Effects_Inferno_Single_Pass_Pixel_Shader_time_0_X", m_fTimeChange);
 	m_pEffect->SetFloat("timeSampleDist", m_fTimeChange);
 	m_pEffect->SetTexture("Flame_Tex", m_pFlame);
 	m_pEffect->SetTexture("Noise_Tex", m_pNoise);
+	m_pEffect->SetMatrix("matWorldViewMatrix", &(matWorld*matView*matProjection));
 	m_pEffect->SetTechnique(hTech);
 	UINT numPasses = 0;
 	m_pEffect->Begin(&numPasses, NULL);
-
 	for (UINT i = 0; i < numPasses; ++i)
 	{
 		m_pEffect->BeginPass(i);
-		
 		m_pMesh->DrawSubset(0);
 		m_pEffect->EndPass();
-		
 	}
-
 	m_pEffect->End();
-
+	numPasses = 0;
+	m_pEffect->Begin(&numPasses, NULL);
+	for (UINT i = 0; i < numPasses; ++i)
+	{
+		m_pEffect->BeginPass(i);
+		m_pMesh->DrawSubset(1);
+		m_pEffect->EndPass();
+	}
+	m_pEffect->End();
+	numPasses = 0;
+	m_pEffect->Begin(&numPasses, NULL);
+	for (UINT i = 0; i < numPasses; ++i)
+	{
+		m_pEffect->BeginPass(i);
+		m_pMesh->DrawSubset(2);
+		m_pEffect->EndPass();
+	}
+	m_pEffect->End();
+	
 	
 
 
