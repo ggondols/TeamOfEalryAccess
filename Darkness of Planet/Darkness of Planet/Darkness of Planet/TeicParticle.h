d5edeb68a287673c8cdf69dc210b134aa0b737ed
인민피시\Tei Kim <kimdigwig@naver.com>
2017-09-27 18:00:18 +0900

    이펙트 매니저 안에 파티클을 만드는 클래스인 particle 과 particle system 클래스 추가했습니다

---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicParticle.h
@@ -0,0 +1,8 @@
+#pragma once
+class TeicParticle
+{
+public:
+	TeicParticle();
+	~TeicParticle();
+};
+
---
+++ Darkness of Planet/Darkness of Planet/Darkness of Planet/TeicParticleSystem.h
@@ -0,0 +1,8 @@
+#pragma once
+class TeicParticleSystem
+{
+public:
+	TeicParticleSystem();
+	~TeicParticleSystem();
+};
+
